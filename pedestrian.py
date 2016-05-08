c42b72ec872a3155cfa2b9c2d50de72ab8002515
Anand Singh Kunwar <anandkunwar95@gmail.com>
2016-05-08 17:01:28 +0530

    Changed line to a function

--- pedestrian.py
+++ pedestrian.py
@@ -3,10 +3,15 @@ import math
 import cv2
 cap = cv2.VideoCapture('TownCentreXVID.avi')
 # fgbg = cv2.createBackgroundSubtractorMOG2(history=5, varThreshold=150)
-fgbg = cv2.bgsegm.createBackgroundSubtractorMOG(history=300)
+fgbg = cv2.bgsegm.createBackgroundSubtractorMOG(history=150, backgroundRatio=0.3)
 
-i = 0
-k = 0
+def line1(x,y):
+    return y - (29*x)/96.0 - 300
+
+def line2(x,y):
+    return y - (29*x)/96.0 - 500
+
+
 crossedAbove = 0
 crossedBelow = 0
 points = set()
@@ -22,74 +27,43 @@ while(1):
     fgmask = cv2.blur(frame, (10,10))
     fgmask = fgbg.apply(fgmask)
     fgmask = cv2.medianBlur(fgmask, 7)
-    # faces = face_cascade.detectMultiScale(frame, 1.3, 5)
-    # for (x,y,w,h) in faces:
-    #     cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
-    if k%25 == 0:
-        # print 'hello'
-        # print w, h
-        # cv2.imwrite('data/image'+str(i)+'.jpg', frame)
-        i+=1
-    k+=1
     oldFgmask = fgmask.copy()
     image, contours, hierarchy = cv2.findContours(fgmask, cv2.RETR_EXTERNAL,1)
-    # # print a
     for contour in contours:
         x,y,w,h = cv2.boundingRect(contour)
         if w>40 and h>90:
             cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2, lineType=cv2.LINE_AA)
-            # print (int(x+w/2.0), int(y+h/2.0))
-            # cv2.circle(frame, (int(x+w/2.0), int(y+h/2.0)), 3, (0,0,255),6)
             point = (int(x+w/2.0), int(y+h/2.0))
             points.add(point)
-        # defects = cv2.convexityDefects(cnt,hull)
-        # start = tuple(cnt[s][0])
-        # end = tuple(cnt[e][0])
-        # far = tuple(cnt[f][0])
-        # cv2.line(frame,start,end,[0,255,0],2)
-        # cv2.circle(frame,far,5,[0,0,255],-1)
     for point in points:
         (xnew, ynew) = point
-        if (ynew - (29*xnew)/96.0 - 300) > 0 and (ynew - (29*xnew)/96.0 - 500) < 0:
+        if line1(xnew, ynew) > 0 and line2(xnew, ynew) < 0:
             pointInMiddle.add(point)
         for prevPoint in prev:
             (xold, yold) = prevPoint
             dist = cv2.sqrt((xnew-xold)*(xnew-xold)+(ynew-yold)*(ynew-yold))
-            # print point
-            # print prevPoint
-            if dist[0] <= 100:
-                # print prevPoint
-                # print point
-                # print dist
-                if ynew - (29*xnew)/96.0 - 300 > 0 and ynew - (29*xnew)/96.0 - 500 < 0:
-                    if yold - (29*xold)/96.0 - 300 < 0: # Point entered from line above
+            if dist[0] <= 120:
+                if line1(xnew, ynew) > 0 and line2(xnew, ynew) < 0:
+                    if line1(xold, yold) < 0: # Point entered from line above
                         pointFromAbove.add(point)
-                    elif yold - (29*xold)/96.0 - 500 > 0: # Point entered from line below
+                    elif line2(xold, yold) > 0: # Point entered from line below
                         pointFromBelow.add(point)
-                        # print '=================='
-                        # print 'Below Point Added'
-                        # print point
-                        # print '=================='
                     else:   # Point was inside the block
                         if prevPoint in pointFromBelow:
                             pointFromBelow.remove(prevPoint)
                             pointFromBelow.add(point)
-                            # print '=================='
-                            # print 'Below Point Updated'
-                            # print point
-                            # print '=================='
 
                         elif prevPoint in pointFromAbove:
                             pointFromAbove.remove(prevPoint)
                             pointFromAbove.add(point)
 
-                if ynew - (29*xnew)/96.0 - 300 < 0 and prevPoint in pointFromBelow: # Point is above the line
+                if line1(xnew, ynew) < 0 and prevPoint in pointFromBelow: # Point is above the line
                     print 'One Crossed Above'
                     print point
                     crossedAbove += 1
                     pointFromBelow.remove(prevPoint)
 
-                if ynew - (29*xnew)/96.0 - 500 > 0 and prevPoint in pointFromAbove: # Point is below the line
+                if line2(xnew, ynew) > 0 and prevPoint in pointFromAbove: # Point is below the line
                     print 'One Crossed Below'
                     print point
                     crossedBelow += 1
@@ -103,26 +77,11 @@ while(1):
             cv2.circle(frame, point, 3, (0,255,255),6)
         else:
             cv2.circle(frame, point, 3, (0,0,255),6)
-        # if point in pointInMiddle:
-        #     cv2.circle(frame, point, 3, (255,0,255),6)
-        # else:
-        #     cv2.circle(frame, point, 3, (0,0,255),6)
-
-    # if contours[1] is not None:
-    #     x,y,w,h = cv2.boundingRect(contours[1])
-    #     print x,y,w,h
-    #     cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
-    # for cont in a:
-
-    #     if type(cont) == np.ndarray:
-    #         x,y,w,h = cv2.boundingRect(cont)
-    #         cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
-    #         # print tuple(cont)
-    #         # cv2.rectangle(frame, tuple(cont[0][0][0]), tuple(cont[0][2][0]), (255,0,0), 2)
     cv2.line(frame, (0,300), (1920,880), (255, 0, 0), 4)
     cv2.line(frame, (0,500), (1920,1080), (255, 0, 0), 4)
-    cv2.putText(frame,'People Crossed Above = '+str(crossedAbove),(1200,50), font, 1,(255,255,255),2,cv2.LINE_AA)
-    cv2.putText(frame,'People Crossed Below = '+str(crossedBelow),(1200,100), font, 1,(255,255,255),2,cv2.LINE_AA)
+    cv2.putText(frame,'People Going Above = '+str(crossedAbove),(1200,50), font, 1,(255,255,255),2,cv2.LINE_AA)
+    cv2.putText(frame,'People Going Below = '+str(crossedBelow),(1200,100), font, 1,(255,255,255),2,cv2.LINE_AA)
+    cv2.imshow('a',oldFgmask)
     cv2.imshow('frame',frame)
     l = cv2.waitKey(1) & 0xff
     if l == 27:
