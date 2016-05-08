62aae8e528fe8535d8a73e598ccd4f5fd324c3a5
Anand Singh Kunwar <anandkunwar95@gmail.com>
2016-05-08 09:08:54 +0530

    Basic Algorithm constructed, fine tuning of parameters left

--- pedestrian.py
+++ pedestrian.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math
 import cv2
 cap = cv2.VideoCapture('TownCentreXVID.avi')
 # fgbg = cv2.createBackgroundSubtractorMOG2(history=5, varThreshold=150)
@@ -6,9 +7,15 @@ fgbg = cv2.bgsegm.createBackgroundSubtractorMOG(history=300)
 
 i = 0
 k = 0
+crossedAbove = 0
+crossedBelow = 0
 points = set()
+pointFromAbove = set()
+pointFromBelow = set()
 while(1):
+    pointInMiddle = set()
     prev = points
+    points = set()
     ret, frame = cap.read()
     fgmask = frame
     fgmask = cv2.blur(frame, (10,10))
@@ -26,20 +33,77 @@ while(1):
     oldFgmask = fgmask.copy()
     image, contours, hierarchy = cv2.findContours(fgmask, cv2.RETR_EXTERNAL,1)
     # # print a
-    for a in contours:
-        x,y,w,h = cv2.boundingRect(a)
+    for contour in contours:
+        x,y,w,h = cv2.boundingRect(contour)
         if w>40 and h>90:
             cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2, lineType=cv2.LINE_AA)
-            print (int(x+w/2.0), int(y+h/2.0))
-            cv2.circle(frame, (int(x+w/2.0), int(y+h/2.0)), 3, (0,0,255),6)
-            points.add((int(x+w/2.0), int(y+h/2.0)))
+            # print (int(x+w/2.0), int(y+h/2.0))
+            # cv2.circle(frame, (int(x+w/2.0), int(y+h/2.0)), 3, (0,0,255),6)
+            point = (int(x+w/2.0), int(y+h/2.0))
+            points.add(point)
         # defects = cv2.convexityDefects(cnt,hull)
         # start = tuple(cnt[s][0])
         # end = tuple(cnt[e][0])
         # far = tuple(cnt[f][0])
         # cv2.line(frame,start,end,[0,255,0],2)
         # cv2.circle(frame,far,5,[0,0,255],-1)
+    for point in points:
+        (xnew, ynew) = point
+        if (ynew - (29*xnew)/96.0 - 300) > 0 and (ynew - (29*xnew)/96.0 - 500) < 0:
+            pointInMiddle.add(point)
+        for prevPoint in prev:
+            (xold, yold) = prevPoint
+            dist = cv2.sqrt((xnew-xold)*(xnew-xold)+(ynew-yold)*(ynew-yold))
+            # print point
+            # print prevPoint
+            if dist[0] <= 100:
+                # print prevPoint
+                # print point
+                # print dist
+                if ynew - (29*xnew)/96.0 - 300 > 0 and ynew - (29*xnew)/96.0 - 500 < 0:
+                    if yold - (29*xold)/96.0 - 300 < 0: # Point entered from line above
+                        pointFromAbove.add(point)
+                    elif yold - (29*xold)/96.0 - 500 > 0: # Point entered from line below
+                        pointFromBelow.add(point)
+                        # print '=================='
+                        # print 'Below Point Added'
+                        # print point
+                        # print '=================='
+                    else:   # Point was inside the block
+                        if prevPoint in pointFromBelow:
+                            pointFromBelow.remove(prevPoint)
+                            pointFromBelow.add(point)
+                            # print '=================='
+                            # print 'Below Point Updated'
+                            # print point
+                            # print '=================='
 
+                        elif prevPoint in pointFromAbove:
+                            pointFromAbove.remove(prevPoint)
+                            pointFromAbove.add(point)
+
+                if ynew - (29*xnew)/96.0 - 300 < 0 and prevPoint in pointFromBelow: # Point is above the line
+                    print 'One Crossed Above'
+                    print point
+                    pointFromBelow.remove(prevPoint)
+
+                if ynew - (29*xnew)/96.0 - 500 > 0 and prevPoint in pointFromAbove: # Point is below the line
+                    print 'One Crossed Below'
+                    print point
+                    pointFromAbove.remove(prevPoint)
+
+
+    for point in points:
+        if point in pointFromBelow:
+            cv2.circle(frame, point, 3, (255,0,255),6)
+        elif point in pointFromAbove:
+            cv2.circle(frame, point, 3, (0,255,255),6)
+        else:
+            cv2.circle(frame, point, 3, (0,0,255),6)
+        # if point in pointInMiddle:
+        #     cv2.circle(frame, point, 3, (255,0,255),6)
+        # else:
+        #     cv2.circle(frame, point, 3, (0,0,255),6)
 
     # if contours[1] is not None:
     #     x,y,w,h = cv2.boundingRect(contours[1])
@@ -52,7 +116,9 @@ while(1):
     #         cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
     #         # print tuple(cont)
     #         # cv2.rectangle(frame, tuple(cont[0][0][0]), tuple(cont[0][2][0]), (255,0,0), 2)
-    # cv2.clipLine(frame, (1,1), (20,20))
+    cv2.line(frame, (0,300), (1920,880), (255, 0, 0), 4)
+    cv2.line(frame, (0,500), (1920,1080), (255, 0, 0), 4)
+    
     cv2.imshow('frame',frame)
     l = cv2.waitKey(1) & 0xff
     if l == 27:
